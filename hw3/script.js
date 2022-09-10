bac4d52e8092bdf882732522f00760ef99f73a5e
Eisen Montalvo <eisen@mac.com>
2022-09-10 13:02:48 -0600

    Added axes to charts

--- hw3/script.js
+++ hw3/script.js
@@ -1,154 +1,244 @@
-// Constants for the charts, that would be useful.
-const CHART_WIDTH = 500
-const CHART_HEIGHT = 250
-const MARGIN = { left: 50, bottom: 20, top: 20, right: 20 }
-const ANIMATION_DURATION = 300
 
+(() => {
+  // Constants for the charts, that would be useful.
+  const CHART_WIDTH = 500
+  const CHART_HEIGHT = 250
+  const MARGIN = { left: 50, bottom: 20, top: 20, right: 20 }
+  const ANIMATION_DURATION = 300
 
-const datasetChanged = (el) => {
-  const newValue = el.target.value
+  const width = 800 - MARGIN.left - MARGIN.right
+  const height = 500 - MARGIN.top - MARGIN.bottom
 
-  console.log(newValue)
-}
+  let dataset = 'covid_utah'
+  let metric = 'deaths'
+  let randomData = false
 
-const metricChanged = (el) => {
-  const newValue = el.target.value
+  const charts = [
+    {
+      id: '#Barchart-div',
+      el: null,
+      update: (el) => updateBarChart(el)
+    },
+    {
+      id: '#Linechart-div',
+      el: null,
+      update: (el) => updateLineChart(el)
+    },
+    {
+      id: '#Areachart-div',
+      el: null,
+      update: (el) => updateAreaChart(el)
+    },
+    {
+      id: '#Scatterplot-div',
+      el: null,
+      update: (el) => updateScatterPlot(el)
+    }
+  ]
 
-  console.log(newValue)
-}
+  const datasetChanged = (el) => {
+    dataset = el.target.value
+    changeData()
+  }
 
-const randomChanged = (el) => {
-  const newValue = el.target.checked
+  const metricChanged = (el) => {
+    metric = el.target.value
+    changeData()
+  }
 
-  console.log(newValue)
-}
+  const randomChanged = (el) => {
+    randomData = el.target.checked
+    changeData()
+  }
 
-function setup() {
+  function setup() {
 
-  // Fill in some d3 setting up here if you need
-  // for example, svg for each chart, g for axis and shapes
+    // Fill in some d3 setting up here if you need
+    // for example, svg for each chart, g for axis and shapes
 
-  document.getElementById('dataset').onchange = datasetChanged
-  document.getElementById('metric').onchange = metricChanged
-  document.getElementById('random').onchange = randomChanged
+    document.getElementById('dataset').onchange = datasetChanged
+    document.getElementById('metric').onchange = metricChanged
+    document.getElementById('random').onchange = randomChanged
+
+    for (const chart of charts) {
+      chart.el = d3.select(chart.id)
+        .append('svg')
+        .attr("width", width + MARGIN.left + MARGIN.right)
+        .attr("height", height + MARGIN.top + MARGIN.bottom)
+        .append('g')
+    }
 
     changeData()
-}
+  }
 
-/**
- * Render the visualizations
- * @param data
- */
-function update(data) {
+  /**
+   * Render the visualizations
+   * @param data
+   */
+  function update(data) {
+    console.log(data)
+    // ****** TODO ******
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(data, el => new Date(el.date)))
+      .range([0, width])
+      .nice()
 
-  // ****** TODO ******
+    const maxY = d3.max(data, el => el[metric])
 
+    const yScale = d3.scaleLinear()
+      .domain([0, maxY])
+      .range([height, 0]) // invert axis
+      .nice()
 
-  // Syntax for line generator.
-  // when updating the path for line chart, use the function as the input for 'd' attribute.
-  // https://github.com/d3/d3-shape/blob/main/README.md
+    const xAxis = d3.axisBottom()
+      .scale(xScale)
+      .tickFormat(d3.timeFormat('%m/%d'))
 
+    const yAxis = d3.axisLeft()
+      .scale(yScale)
 
-  // const lineGenerator = d3.line()
-  //   .x(d => the x coordinate for a point of the line)
-  //   .y(d => the y coordinate for a point of the line);
-
-  // Syntax for area generator.
-  // the area is bounded by upper and lower lines. So you can specify x0, x1, y0, y1 seperately. Here, since the area chart will have upper and lower sharing the x coordinates, we can just use x(). 
-  // Similarly, use the function as the input for 'd' attribute. 
-
-  // const areaGenerator = d3.area()
-  //   .x(d => the x coordinates for upper and lower lines, both x0 and x1)
-  //   .y1(d => the y coordinate for the upper line)
-  //   .y0(d=> the base line y coordinate for the area);
-
-
-  //Set up scatter plot x and y axis. 
-  //Since we are mapping death and case, we need new scales instead of the ones above. 
-  //Cases would be the horizontal axis, so we need to use width related constants.
-  //Deaths would be vertical axis, so that would need to use height related constants.
+    // Syntax for line generator.
+    // when updating the path for line chart, use the function as the input for 'd' attribute.
+    // https://github.com/d3/d3-shape/blob/main/README.md
 
 
-  //TODO 
-  // call each update function below, adjust the input for the functions if you need to.
-}
+    // const lineGenerator = d3.line()
+    //   .x(d => the x coordinate for a point of the line)
+    //   .y(d => the y coordinate for a point of the line);
 
-/**
- * Update the bar chart
- */
+    // Syntax for area generator.
+    // the area is bounded by upper and lower lines. So you can specify x0, x1, y0, y1 seperately. 
+    // Here, since the area chart will have upper and lower sharing the x coordinates, we can just use x(). 
+    // Similarly, use the function as the input for 'd' attribute. 
 
-function updateBarChart() {
-
-}
-
-/**
- * Update the line chart
- */
-function updateLineChart() {
-
-}
-
-/**
- * Update the area chart 
- */
-function updateAreaChart() {
-
-}
-
-/**
- * update the scatter plot.
- */
-
-function updateScatterPlot() {
-
-}
+    // const areaGenerator = d3.area()
+    //   .x(d => the x coordinates for upper and lower lines, both x0 and x1)
+    //   .y1(d => the y coordinate for the upper line)
+    //   .y0(d=> the base line y coordinate for the area);
 
 
-/**
- * Update the data according to document settings
- */
-function changeData() {
-  //  Load the file indicated by the select menu
-  const dataFile = d3.select('#dataset').property('value')
+    //Set up scatter plot x and y axis. 
+    //Since we are mapping death and case, we need new scales instead of the ones above. 
+    //Cases would be the horizontal axis, so we need to use width related constants.
+    //Deaths would be vertical axis, so that would need to use height related constants.
 
-  d3.csv(`data/${dataFile}.csv`)
-    .then(dataOutput => {
+    const maxX = d3.max(data, el => el.cases)
 
-      /**
-       * D3 loads all CSV data as strings. While Javascript is pretty smart
-       * about interpreting strings as numbers when you do things like
-       * multiplication, it will still treat them as strings where it makes
-       * sense (e.g. adding strings will concatenate them, not add the values
-       * together, or comparing strings will do string comparison, not numeric
-       * comparison).
-       *
-       * We need to explicitly convert values to numbers so that comparisons work
-       * when we call d3.max()
-       **/
+    const xScaleScatter = d3.scaleLinear()
+      .domain([0, maxX])
+      .range([0, width]) // invert axis
+      .nice()
 
-      const dataResult = dataOutput.map((d) => ({
-        cases: parseInt(d.cases),
-        deaths: parseInt(d.deaths),
-        date: d3.timeFormat("%m/%d")(d3.timeParse("%d-%b")(d.date))
-      }))
-      if (document.getElementById('random').checked) {
-        // if random subset is selected
-        update(randomSubset(dataResult))
+    const xAxisScatter = d3.axisBottom()
+      .scale(xScaleScatter)
+
+    //TODO 
+    // call each update function below, adjust the input for the functions if you need to.
+    charts.map(chart => {
+      // Scatter plot uses a different horizontal axis
+      if (chart.id === '#Scatterplot-div') {
+        chart.xAxis = xAxisScatter
       } else {
-        update(dataResult)
+        chart.xAxis = xAxis
       }
-    }).catch(e => {
-      console.log(e)
-      alert('Error!')
+
+      // Clear old axis
+      chart.el.selectAll('g').remove()
+
+      // Add horizontal axis
+      chart.el.append('g')
+        .classed('x-axis', true)
+        .attr('transform', `translate(${MARGIN.left}, ${height + MARGIN.top})`)
+        .call(chart.xAxis)
+
+      // Add vertical axis
+      chart.yAxis = yAxis
+      chart.el.append('g')
+        .attr('transform', `translate(${MARGIN.left}, ${MARGIN.top})`)
+        .call(chart.yAxis)
+
+      chart.update(chart)
     })
-}
+  }
 
-/**
- *  Slice out a random chunk of the provided in data
- *  @param data
- */
-function randomSubset(data) {
-  return data.filter((d) => Math.random() > 0.5)
-}
+  /**
+   * Update the bar chart
+   */
 
-setup()
+  function updateBarChart(chart) {
+
+  }
+
+  /**
+   * Update the line chart
+   */
+  function updateLineChart(chart) {
+
+  }
+
+  /**
+   * Update the area chart 
+   */
+  function updateAreaChart(chart) {
+
+  }
+
+  /**
+   * update the scatter plot.
+   */
+
+  function updateScatterPlot(chart) {
+
+  }
+
+
+  /**
+   * Update the data according to document settings
+   */
+  function changeData() {
+    //  Load the file indicated by the select menu
+    // const dataFile = d3.select('#dataset').property('value')
+
+    d3.csv(`data/${dataset}.csv`)
+      .then(dataOutput => {
+
+        /**
+         * D3 loads all CSV data as strings. While Javascript is pretty smart
+         * about interpreting strings as numbers when you do things like
+         * multiplication, it will still treat them as strings where it makes
+         * sense (e.g. adding strings will concatenate them, not add the values
+         * together, or comparing strings will do string comparison, not numeric
+         * comparison).
+         *
+         * We need to explicitly convert values to numbers so that comparisons work
+         * when we call d3.max()
+         **/
+
+        const dataResult = dataOutput.map((d) => ({
+          cases: parseInt(d.cases),
+          deaths: parseInt(d.deaths),
+          date: d3.timeFormat("%m/%d")(d3.timeParse("%d-%b")(d.date))
+        }))
+        //if (document.getElementById('random').checked) {
+        if (randomData) {
+          // if random subset is selected
+          update(randomSubset(dataResult))
+        } else {
+          update(dataResult)
+        }
+      }).catch(e => {
+        console.log(e)
+        alert('Error!')
+      })
+  }
+
+  /**
+   *  Slice out a random chunk of the provided in data
+   *  @param data
+   */
+  function randomSubset(data) {
+    return data.filter((d) => Math.random() > 0.5)
+  }
+
+  setup()
+})()
