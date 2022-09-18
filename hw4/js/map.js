02f07ed5810950517525e7243cde9f89a2d62627
Eisen Montalvo <eisen@mac.com>
2022-09-18 11:48:37 -0600

    Render Map with countries and graticules

--- hw4/js/map.js
+++ hw4/js/map.js
@@ -1,20 +1,93 @@
 /** Class representing the map view. */
 class MapVis {
+
+  svg = null
+
   /**
    * Creates a Map Visuzation
    * @param globalApplicationState The shared global application state (has the data and the line chart instance in it)
    */
   constructor(globalApplicationState) {
-    this.globalApplicationState = globalApplicationState;
+    this.globalApplicationState = globalApplicationState
+
+    this.svg = d3.select('#map')
 
     // Set up the map projection
     const projection = d3.geoWinkel3()
       .scale(150) // This set the size of the map
-      .translate([400, 250]); // This moves the map to the center of the SVG
+      .translate([400, 250]) // This moves the map to the center of the SVG
 
+    let path = d3.geoPath()
+      .projection(projection)
+
+    let ule = this.svg.select('#graticules')
+      .append('g')
+      .classed('ule', true)
+
+    let outline = this.svg.select('#graticules')
+      .append('g')
+      .classed('outline', true)
+
+    let graticuleGen = d3.geoGraticule()
+
+    let graticuleUle = graticuleGen.lines()
+    let graticuleOutline = graticuleGen.outline()
+
+    const lineGen = d3.line()
+      .x(el => projection(el)[0])
+      .y(el => projection(el)[1])
+
+    ule.selectAll('path')
+      .data(graticuleUle)
+      .join(
+        (enter) => {
+          return enter.append('path')
+            .attr('d', path)
+            .attr('fill', 'none')
+            .attr('stroke', 'lightgray')
+        },
+        (update) => {
+
+        },
+        (exit) => { }
+      )
+
+    outline.selectAll('path')
+      .data(graticuleOutline.coordinates)
+      .join(
+        (enter) => {
+          return enter.append('path')
+            .datum(graticuleOutline)
+            .attr('d', path)
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 2)
+        },
+        (update) => {
+
+        },
+        (exit) => { }
+      )
+
+    let countries = this.svg.select('#countries')
+
+    countries.selectAll('path')
+      .data(this.globalApplicationState.mapData.features)
+      .join(
+        (enter) => {
+          return enter.append('path')
+            .attr('d', path)
+            .attr('fill', 'white')
+            .attr('stroke', 'black')
+        },
+        (update) => {
+
+        },
+        (exit) => { }
+      )
   }
 
-  updateSelectedCountries () {
+  updateSelectedCountries() {
 
   }
 }
--- hw4/js/script.js
+++ hw4/js/script.js
@@ -1,38 +1,42 @@
-// ******* DATA LOADING *******
-// We took care of that for you
-async function loadData () {
-  const covidData = await d3.csv('data/owid-covid.csv');
-  const mapData = await d3.json('data/world.json');
-  return { covidData, mapData };
-}
+(() => {
+  // ******* DATA LOADING *******
+  // We took care of that for you
+  async function loadData() {
+    const covidData = await d3.csv('data/owid-covid.csv')
+    const mapData = await d3.json('data/world.json')
+    return { covidData, mapData }
+  }
 
 
-// ******* STATE MANAGEMENT *******
-// This should be all you need, but feel free to add to this if you need to 
-// communicate across the visualizations
-const globalApplicationState = {
-  selectedLocations: [],
-  covidData: null,
-  mapData: null,
-  worldMap: null,
-  lineChart: null,
-};
+  // ******* STATE MANAGEMENT *******
+  // This should be all you need, but feel free to add to this if you need to 
+  // communicate across the visualizations
+  const globalApplicationState = {
+    selectedLocations: [],
+    covidData: null,
+    mapData: null,
+    worldMap: null,
+    lineChart: null,
+  }
 
 
-//******* APPLICATION MOUNTING *******
-loadData().then((loadedData) => {
-  console.log('Here is the imported data:', loadedData.covidData);
+  //******* APPLICATION MOUNTING *******
+  loadData().then((loadedData) => {
+    // console.log('Here is the imported data:', loadedData.covidData)
 
-  // Store the loaded data into the globalApplicationState
-  globalApplicationState.covidData = loadedData.covidData;
-  globalApplicationState.mapData = loadedData.mapData;
+    // Store the loaded data into the globalApplicationState
+    globalApplicationState.covidData = loadedData.covidData
 
-  // Creates the view objects with the global state passed in 
-  const worldMap = new MapVis(globalApplicationState);
-  const lineChart = new LineChart(globalApplicationState);
+    globalApplicationState.mapData = 
+      topojson.feature(loadedData.mapData, loadedData.mapData.objects.countries) 
 
-  globalApplicationState.worldMap = worldMap;
-  globalApplicationState.lineChart = lineChart;
+    // Creates the view objects with the global state passed in 
+    const worldMap = new MapVis(globalApplicationState)
+    const lineChart = new LineChart(globalApplicationState)
 
-  //TODO add interactions for Clear Selected Countries button
-});
+    globalApplicationState.worldMap = worldMap
+    globalApplicationState.lineChart = lineChart
+
+    //TODO add interactions for Clear Selected Countries button
+  })
+})()
