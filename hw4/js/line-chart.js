260b38cacd70fb579608c61de8d3676ec423ff9b
Eisen Montalvo <eisen@mac.com>
2022-09-18 19:21:05 -0600

    Group data by continents

--- hw4/js/line-chart.js
+++ hw4/js/line-chart.js
@@ -1,14 +1,22 @@
 /** Class representing the line chart view. */
 class LineChart {
+
+  continents = null
+
   /**
    * Creates a LineChart
    * @param globalApplicationState The shared global application state (has the data and map instance in it)
    */
   constructor(globalApplicationState) {
     // Set some class level variables
-    this.globalApplicationState = globalApplicationState;
+    this.globalApplicationState = globalApplicationState
+
+    this.continents = d3.group(globalApplicationState.covidData.filter(this.GetContinentData), el => el.location)
+    console.log(this.continents)
 
   }
+
+  GetContinentData = el => el.iso_code.startsWith('OWID')
 
   updateSelectedCountries () {
 
