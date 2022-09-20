2c672733706b5540212d58bb8f7669b0f4f3634a
Eisen Montalvo <eisen@mac.com>
2022-09-19 22:48:28 -0600

    Added Axes labels

--- hw4/js/line-chart.js
+++ hw4/js/line-chart.js
@@ -4,7 +4,7 @@ class LineChart {
   continents = null
   chartSVG = null
 
-  width = 800
+  width = 700
   height = 500
 
   left = 80
@@ -35,8 +35,20 @@ class LineChart {
     const xAxis = d3.select('#x-axis')
       .attr('transform', `translate(${this.left}, ${this.height - this.bottom})`)
 
+    xAxis.append('text')
+      .attr('transform', `translate(${(this.width - this.left) / 2}, 40)`)
+      .attr('fill', 'black')
+      .attr('text-anchor', 'middle')
+      .text('Date')
+
     const yAxis = d3.select('#y-axis')
       .attr('transform', `translate(${this.left}, ${this.top})`)
+
+    yAxis.append('text')
+      .attr('transform', `translate(-60, ${(this.height - this.bottom) / 2}) rotate(-90)`)
+      .attr('fill', 'black')
+      .attr('text-anchor', 'middle')
+      .text('Cases per million')
 
     const earliestDate = this.EarliestDate(this.continents)
     const greatestDate = this.GreatestDate(this.continents)
@@ -49,7 +61,7 @@ class LineChart {
     this.xAxis.scale(xScale)
 
     xAxis.transition()
-        .call(this.xAxis)
+      .call(this.xAxis)
 
 
     const maxY = this.GreatestValue(this.continents)
@@ -62,45 +74,45 @@ class LineChart {
     this.yAxis.scale(yScale)
 
     yAxis.transition()
-        .call(this.yAxis)
+      .call(this.yAxis)
 
   }
 
   GreatestValue = (group) => {
     let greatestValue = d3.greatest(group, ([cases, value]) => {
-      const greatest = d3.greatest(d3.group(value, el => el.total_cases_per_million), ([icases, ]) => {
+      const greatest = d3.greatest(d3.group(value, el => el.total_cases_per_million), ([icases,]) => {
         return parseFloat(icases)
       })
       return parseFloat(greatest[0])
     })
 
-    return parseFloat(greatestValue[1].reduce( (prev, next) => {
+    return parseFloat(greatestValue[1].reduce((prev, next) => {
       return parseFloat(prev.total_cases_per_million) > parseFloat(next.total_cases_per_million) ? prev : next
     }, greatestValue[0]).total_cases_per_million)
   }
 
   EarliestDate = (group) => {
     let earliestDate = d3.least(group, ([date, value]) => {
-      const least = d3.least(d3.group(value, el => el.date), ([idate, ]) => {
+      const least = d3.least(d3.group(value, el => el.date), ([idate,]) => {
         return new Date(idate).valueOf()
       })
       return new Date(least[0]).valueOf()
     })
 
-    return new Date(earliestDate[1].reduce( (prev, next) => {
+    return new Date(earliestDate[1].reduce((prev, next) => {
       return new Date(prev.date) < new Date(next.date) ? prev : next
     }, earliestDate[0]).date)
   }
 
   GreatestDate = (group) => {
     let greatestDate = d3.greatest(group, ([date, value]) => {
-      const greatest = d3.greatest(d3.group(value, el => el.date), ([idate, ]) => {
+      const greatest = d3.greatest(d3.group(value, el => el.date), ([idate,]) => {
         return new Date(idate).valueOf()
       })
       return new Date(greatest[0]).valueOf()
     })
 
-    return new Date(greatestDate[1].reduce( (prev, next) => {
+    return new Date(greatestDate[1].reduce((prev, next) => {
       return new Date(prev.date) > new Date(next.date) ? prev : next
     }, greatestDate[0]).date)
   }
