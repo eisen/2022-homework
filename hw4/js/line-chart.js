ab7348800b88429a494a99bae131b22e8a8c2125
Eisen Montalvo <eisen@mac.com>
2022-09-21 14:34:25 -0600

    Added labels to cursor line

--- hw4/js/line-chart.js
+++ hw4/js/line-chart.js
@@ -4,6 +4,7 @@ class LineChart {
   continents = null
   selData = null
   chartSVG = null
+  content = null
 
   width = 700
   height = 500
@@ -37,6 +38,18 @@ class LineChart {
     this.continents = d3.group(globalApplicationState.covidData.filter(this.GetContinentData), el => el.location)
 
     this.chartSVG = d3.select('#line-chart')
+
+    this.content = this.chartSVG.select('#lines')
+
+    this.content.append('rect')
+      .attr('x', this.left)
+      .attr('y', this.top)
+      .attr('width', this.width - this.left - this.right)
+      .attr('height', this.height - this.top - this.bottom)
+      .attr('fill', 'white')
+      .on('mousemove', (el) => {
+        this.UpdateOverlay(el)
+      })
 
     this.SetupAxes()
     this.updateSelectedCountries()
@@ -82,6 +95,8 @@ class LineChart {
   }
 
   GetContinentData = el => el.iso_code.startsWith('OWID')
+
+  GetCountrytData = el => el.iso_code.startsWith('OWID') === false
 
   SetupAxes = () => {
     this.xAxisGroup = d3.select('#x-axis')
@@ -132,19 +147,67 @@ class LineChart {
       .call(this.yAxis)
   }
 
-  LeftSide = (x) => x - this.left < (this.width - this.left - this.right) * 0.5
-  ? true : false
+  OnLeftSide = (x) => x - this.left < (this.width - this.left - this.right) * 0.5
+    ? true : false
 
   UpdateOverlay = (el) => {
-      const x = el.offsetX
-      const overlay = d3.select('#overlay line')
+    const x = el.offsetX
+    d3.select('#overlay line')
       .attr('stroke', 'black')
       .attr('y1', 0)
       .attr('y2', this.height - this.bottom)
       .attr('x1', x)
       .attr('x2', x)
 
-      const date = this.xScale.invert(x - this.left)
+    const names = Array.from(this.selData, ([key,]) => key)
+    const date = this.xScale.invert(x - this.left)
+    const labels = []
+
+    const year = date.toLocaleString("default", { year: "numeric" })
+    const month = date.toLocaleString("default", { month: "2-digit" })
+    const day = date.toLocaleString("default", { day: "2-digit" })
+
+    names.forEach(el => {
+      const cases = this.selData.get(el).filter(el => {
+        return el.date === `${year}-${month}-${day}`
+      })[0].total_cases_per_million
+
+      labels.push({
+        location: el,
+        cases: parseFloat(cases)
+      })
+    })
+
+    labels.sort((a, b) => b.cases - a.cases) // Descending sort
+
+    const text_format = d3.formatPrefix(',.1', 1e3)
+    d3.select('#overlay')
+      .selectAll('text')
+      .data(labels)
+      .join(
+        (enter) => {
+          enter.append('text')
+            .text(el => `${el.location}, ${text_format(el.cases)}`)
+            .attr('x', this.OnLeftSide(x) ? x + 10 : x - 10)
+            .attr('y', (el, idx) => this.top + idx * 20 + 10)
+            .attr('text-anchor', this.OnLeftSide(x) ? 'start' : 'end')
+            .attr('fill', 'black')
+            .attr('opacity', 1)
+        },
+        (update) => {
+          update.text(el => `${el.location}, ${text_format(el.cases)}`)
+            .attr('x', this.OnLeftSide(x) ? x + 10 : x - 10)
+            .attr('y', (el, idx) => this.top + idx * 20 + 10)
+            .attr('text-anchor', this.OnLeftSide(x) ? 'start' : 'end')
+        },
+        (exit) => {
+          exit.transition()
+          .duration(this.animationDuration)
+          .attr('opacity', 0)
+          .remove()
+        }
+      )
+
   }
 
   RenderLines = (data) => {
@@ -152,19 +215,7 @@ class LineChart {
       .x(el => this.xScale(new Date(el.date)) + this.left)
       .y(el => this.yScale(el.total_cases_per_million) + this.top)
 
-    const content = this.chartSVG.select('#lines')
-
-    content.append('rect')
-    .attr('x', this.left)
-    .attr('y', this.top)
-    .attr('width', this.width - this.left - this.right)
-    .attr('height', this.height - this.top - this.bottom)
-    .attr('fill', 'white')
-    .on('mousemove', (el) => {
-      this.UpdateOverlay(el)
-    })
-
-    content.selectAll('path')
+    this.content.selectAll('path')
       .data(data) // create a one object array to activate the join
       .join(
         (enter) => {
@@ -197,17 +248,17 @@ class LineChart {
   updateSelectedCountries() {
     const sel = this.globalApplicationState.selectedLocations
     const worldMap = this.globalApplicationState.worldMap
+
+    d3.selectAll('#overlay text').remove()
+    d3.select('#overlay line').attr('x1', this.left).attr('x2', this.left)
+
     if (sel.length > 0) {
-      this.selData = []
-      // Update charts with selection
-      sel.forEach((el) => {
-        selData.push([el, worldMap.CasesForCountry(el)])
-      })
-      this.RenderAxes(this.selData)
-      this.RenderLines(this.selData)
+      this.selData = d3.group(this.globalApplicationState.covidData.filter(this.GetCountrytData)
+        .filter(el => sel.includes(el.iso_code)), el => el.location)
     } else {
-      this.RenderAxes(this.continents)
-      this.RenderLines(this.continents)
+      this.selData = this.continents
     }
+    this.RenderAxes(this.selData)
+    this.RenderLines(this.selData)
   }
 }
