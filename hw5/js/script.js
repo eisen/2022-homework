f77f8295ede5387082256b3888407a232a97d1eb
Eisen Montalvo <eisen@mac.com>
2022-10-01 23:22:57 -0600

    Extra Credit 1 (Data Loading)
    Data is loaded from the CSV file. JSON file isn't used.

--- hw5/js/script.js
+++ hw5/js/script.js
@@ -1,22 +1,35 @@
-preGrouped = d3.json('./data/senate_polls.json');
-// extraCredit = d3.csv('./data/senate_polls.csv');
+preGrouped = d3.json('./data/senate_polls.json')
+extraCredit = d3.csv('./data/senate_polls.csv')
 
-Promise.all([d3.csv('./data/senate_forecasts.csv'), preGrouped]).then( data =>
-    {
-        let forecastData = data[0];
-        let pollData = data[1];
+byPercentage = (c1, c2) => parseFloat(c1.pct) > parseFloat(c2.pct) ? c1 : c2
 
-        /////////////////
-        // EXTRA CREDIT//
-        /////////////////
-        /**
-         * replace preGrouped with extraCredit and uncomment the line that defines extraCredit.
-         * Then use d3.rollup to group the csvfile on the fly.
-         * 
-         * If you are not doing the extra credit, you do not need to change this file.
-         */
-        
-        rolledPollData = new Map(pollData); //  convert to a Map object for consistency with d3.rollup
-        let table = new Table(forecastData, rolledPollData);
-        table.drawTable();
-    });
+Promise.all([d3.csv('./data/senate_forecasts.csv'), extraCredit, preGrouped]).then(data => {
+    let forecastData = data[0]
+    let pollData = d3.rollups(data[1], v => {
+        const result = {
+            name: v[0].pollster,
+            margin: 0,
+            state: v[0].state
+        }
+
+        const repMargin = v.filter( d => d.party === 'REP').reduce( byPercentage, { pct: "0"})
+        const demMargin = v.filter( d => d.party === 'DEM').reduce( byPercentage, { pct: "0"})
+        result.margin = repMargin.pct - demMargin.pct
+
+        return result
+    }, d => d.state, d => d.poll_id).map( el => [el[0], el[1].map( p => p[1])])
+
+    /////////////////
+    // EXTRA CREDIT//
+    /////////////////
+    /**
+     * replace preGrouped with extraCredit and uncomment the line that defines extraCredit.
+     * Then use d3.rollup to group the csvfile on the fly.
+     * 
+     * If you are not doing the extra credit, you do not need to change this file.
+     */
+
+    rolledPollData = new Map(pollData) //  convert to a Map object for consistency with d3.rollup
+    let table = new Table(forecastData, rolledPollData)
+    table.drawTable()
+})
